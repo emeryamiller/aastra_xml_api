004f1a8a732b2161af78ffcdddf561ee2299b19d
Emery A. Miller <emery.miller@easyofficephone.com>
2014-09-11 15:57:15 -0400

    Fix requires and properly bump version

--- lib/aastra_xml_api.rb
+++ lib/aastra_xml_api.rb
@@ -1,7 +1,9 @@
 require 'cgi'
 
-(Dir['lib/aastra_xml_api/*'] - ['lib/aastra_xml_api/phone_gd_image.rb']).sort.each do |path|
-  name = File.split(path)[1][0..-4]
+%w(version array_extensions phone phone_configuration phone_configuration_entry phone_directory phone_directory_entry
+   phone_execute phone_execute_entry phone_formatted_text_screen phone_formatted_text_screen_entry phone_icon_entry
+   phone_image_menu phone_image_menu_entry phone_image_screen phone_input_screen phone_input_screen_entry
+   phone_softkey_entry phone_status phone_status_entry phone_text_menu phone_text_menu_entry phone_text_screen).each do |name|
   require "aastra_xml_api/#{name}"
 end
 
